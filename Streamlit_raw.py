b4b303576749134da885e24e07ac00ff89af09fb
Thomas-Witt95 <TomTheChef.ZA@Gmail.Com>
2025-05-26 15:38:45 +0100

    miserable git update
    miserable git update

--- Streamlit_raw.py
+++ Streamlit_raw.py
@@ -2,16 +2,25 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+# Set the page configuration
 
 
-st.title('Simple Title Here')
-st.header('This is a Header')
-st.subheader('This is a Subheader')
-st.markdown('This is _Markdown_')
-st.caption('Captions are small text')
+st.title('Spotify Data Analysis')
+st.header('Miserble git makes DF_mega')
+st.subheader('his kids were trying to feed him soming they made')
+st.markdown('He was _Not Pleased_')
+st.caption('TBF it looked a bit gross')
 
 code_example = """
  def greet(name):
    print('Hello', name)"""
 
 st.code(code_example, language="python")
+
+st.markdown('hello')
+
+bar = px.bar(x=[1, 2, 3], y=[4, 5, 6], labels={'x': 'X Axis', 'y': 'Y Axis'}, title='Sample Bar Chart')
+bar.show()
+

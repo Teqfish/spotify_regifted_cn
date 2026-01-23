c761b5dd96a7cecc34f9ed1c8898b309d2551250
Charlie Nash <iamcharlienash@gmail.com>
2026-01-23 01:07:32 +0000

    faqs update

--- app.py
+++ app.py
@@ -8257,22 +8257,22 @@ elif page == "FAQs":
         st.markdown('''
             <ul>
                 <li>This app began as a small, locally run dashboard built by a
-                    four-person team as a final project for a data analytics course.</li>
+                four-person team as a final project for a data analytics course.</li>
 
                 <li>That team comprised of Charlie Nash (me), Ben Garalnick, Jana Hueppe, and Tom Witt.</li>
 
                 <li>That first version used a simple upload/ETL flow and a handful of visualizations
-                    — top artists, albums, genres, a yearly timeline — and some light enrichment to pull artwork
-                    and Spotify popularity via APIs.</li>
+                — top artists, albums, genres, a yearly timeline — and some light enrichment to pull artwork
+                and Spotify popularity via APIs.</li>
 
                 <li>We ran it on a teammate’s laptop, stored data in local CSVs, and learned a lot about version control,
-                    conflict resolution, and shipping something that worked — even if only for a demo.</li>
+                conflict resolution, and shipping something that worked — even if only for a demo.</li>
 
                 <li>For this deployed version, I rewrote the whole thing and added a ton of new features.
-                    It now includes authentication and cookies, automated ETL and data enrichment using Spotify and Discogs,
-                    a genre-mapping system that collapses 6,000+ labels into 25 “supergenres” (and auto-fills gaps via LLM prompts),
-                    expanded popularity and chart scoring with richer visuals, and a statistical deep-dive into the
-                    relationships between normality, genres, and time.</li>
+                It now includes authentication and cookies, automated ETL and data enrichment using Spotify and Discogs,
+                a genre-mapping system that collapses 6,000+ labels into 25 “supergenres” (and auto-fills gaps via LLM prompts),
+                expanded popularity and chart scoring with richer visuals, and a statistical deep-dive into the
+                relationships between normality, genres, and time.</li>
             </ul>''',unsafe_allow_html=True)
     with st.expander('3. How did you calculate "Sheepleness"?'):
         st.markdown('''
